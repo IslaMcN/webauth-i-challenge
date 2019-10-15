b25673654fe8d8c78d771da11f4c98364efbb6c6
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2019-10-15 12:18:22 -0700

    auth router donee

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const Users = require('../users/users-model');
+const router = require('express').Router();
 
 router.post('/register', (req, res) => {
     let user = req.body;
@@ -10,4 +11,41 @@ router.post('/register', (req, res) => {
     .then(save => {
         res.status(201).json(save);
     })
-})
+    .catch(err => {
+        res.status(500).json(err)
+    });
+});
+
+router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+    Users.findBy({username})
+    .first()
+    .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+            req.session.username = user.username;
+            res.status(200).json({
+                message: `You may enter. . . ${user.username}`,
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+router.get('/logout', (req, res) => {
+    if (req.session) {
+        req.session.destroy(err => {
+            res.status(200)
+            .json({
+                message: 'Bye Felicia!'
+            });
+        });
+    }else{
+        res.status(200).json({
+            message: 'Who are you?'
+        });
+    }
+});
+
+module.exports = router;
