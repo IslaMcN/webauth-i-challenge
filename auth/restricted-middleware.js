10e0a69041ea9522f02b9d6c1428b1ba48681449
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2019-10-15 12:22:01 -0700

    updated middleware

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,9 @@
-const bcrypt = require('bcryptjs');
-const Users = require('../users/users-model')
 module.exports = (req, res, next) => {
-    const { username, password } = req.headers;
-    if(username && password) {
-        Users.findBy({ username })
-        .first()
-        .then(user => {
-            if (user && bcrypt.compareSync(password, user.password)) {
-                next();
-            }else{
-                res.status(401).json({
-                    message: 'Nope'
-                });
-            }
+    if (req.session && req.session.username){
+        next();
+    } else {
+        res.status(401).json({
+            message: 'Dont even think about it'
         })
-        .catch(err => {
-            res.status(500).json({ message: 'Ran into an error'})
-        });
-
-    }else{
-        res.status(400).json({ message: 'no'});
     }
 };
